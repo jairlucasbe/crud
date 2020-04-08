5dc36439ed1833813d5c41fa1c2754ddd5bb2e12
kadrrc <56481823+kadrrc@users.noreply.github.com>
2020-04-08 00:49:52 -0500

    Add files via upload

--- crud.py
+++ crud.py
@@ -1,17 +1,75 @@
 from tkinter import *
+import sqlite3
+from tkinter import messagebox
+
+#*********************** GLOBALES ***************************
 root = Tk()
-#**************** CREACION DEL MENU *************************
+nombre_create = StringVar()
+password_create = StringVar()
+apellido_create = StringVar()
+direccion_create = StringVar()
+comentario_create = StringVar()
+#********************** FUNCIONES ***************************
+#------------------------- funciones bbdd -------------------
+def conectar():
+	try:
+		conexion = sqlite3.connect("base")
+		cursor = conexion.cursor()
+		cursor.execute('''
+			CREATE TABLE usuarios (
+			ID integer PRIMARY KEY AUTOINCREMENT,
+			NOMBRE varchar(50),
+			CONTRASEÑA varchar(50),
+			APELLIDO varchar(50),
+			DIRECCION varchar(50),
+			COMENTARIO varchar(100)) 
+		''')
+		conexion.commit()
+		conexion.close()
+		messagebox.showinfo("Confirmacion","base de datos creada")
+	except sqlite3.OperationalError as e:
+		messagebox.showwarning("Operacion incorrecta","base de datos existente")
+#-----------------------------------------------------------------------------------------------
+def salir():
+	valor=messagebox.askquestion("Mensaje de confirmación","Deseas salir de la aplicacion?")
+	if valor=="yes":
+		root.destroy()
+#-------------------------------- FUNCIONES DE BORRAR -----------------------------------------
+def borrar():
+	id_entry.delete(0,END)
+	nombre_entry.delete(0,END)
+	password_entry.delete(0,END)
+	apellido_entry.delete(0,END)
+	direccion_entry.delete(0,END)
+	comentario_text.delete(1.0,END)
+
+#********************************** FUNCIONES DE CRUD ************************************
+#------------------------------------- CREATE ------------------------------------------
+def create():
+	conexion = sqlite3.connect("base")
+	cursor = conexion.cursor()
+	cursor.execute("INSERT INTO usuarios (NOMBRE,CONTRASEÑA,APELLIDO,DIRECCION,COMENTARIO) VALUES(NULL,?,?,?,?,?)",(nombre_create.get(),password_create.get(),apellido_create.get(),direccion_create.get(),comentario_create.get()))
+	messagebox.showinfo("Confirmacion","registro insertado")
+	conexion.commit()
+	conexion.close()
+#------------------------------------- READ ------------------------------------------
+#------------------------------------- UPDATE ------------------------------------------
+#------------------------------------- DELETE ------------------------------------------
+
+
+
+#********************************** CREACION DEL MENU ************************************
 menubar = Menu(root)
 root.config(menu=menubar)
 menu_bbdd=Menu(menubar,tearoff=0)
-menu_bbdd.add_command(label="conectar")
-menu_bbdd.add_command(label="salir")
+menu_bbdd.add_command(label="conectar",command=conectar)
+menu_bbdd.add_command(label="salir",command=salir)
 
 menu_borrar=Menu(menubar,tearoff=0)
-menu_borrar.add_command(label="borrar campos")
+menu_borrar.add_command(label="borrar campos",command=borrar)
 
 menu_crud=Menu(menubar,tearoff=0)
-menu_crud.add_command(label="create")
+menu_crud.add_command(label="create",command=create)
 menu_crud.add_command(label="read")
 menu_crud.add_command(label="update")
 menu_crud.add_command(label="delete")
@@ -29,7 +87,7 @@ menubar.add_cascade(label="ayuda",menu=menu_ayuda)
 frame=Frame(root)
 frame.pack()
 #---------------------- LABEL ------------------------------
-id_label = Label(frame,text="id",padx=1,pady=10,justify="center")
+id_label = Label(frame,text="id",padx=1,pady=10)
 id_label.grid(row=0,column=0)
 nombre_label = Label(frame,text="nombre",padx=1,pady=10,justify="center")
 nombre_label.grid(row=1,column=0)
@@ -44,16 +102,16 @@ comentario_label.grid(row=5,column=0)
 #--------------------- Entry -------------------------------
 id_entry=Entry(frame)
 id_entry.grid(row=0,column=1,padx=15,pady=10,columnspan=3)
-nombre_entry=Entry(frame)
+nombre_entry=Entry(frame,justify="right",fg="red",textvariable=nombre_create)
 nombre_entry.grid(row=1,column=1,padx=10,pady=10,columnspan=3)
-password_entry=Entry(frame)
+password_entry=Entry(frame,show="*",textvariable=password_create)
 password_entry.grid(row=2,column=1,padx=10,pady=10,columnspan=3)
-apellido_entry=Entry(frame)
+apellido_entry=Entry(frame,textvariable=apellido_create)
 apellido_entry.grid(row=3,column=1,padx=10,pady=10,columnspan=3)
-direccion_entry=Entry(frame)
+direccion_entry=Entry(frame,textvariable=direccion_create)
 direccion_entry.grid(row=4,column=1,padx=10,pady=10,columnspan=3)
 #------------------------- text y scroll-----------------------
-comentario_text = Text(frame,width=15,height=4)
+comentario_text = Text(frame,width=15,height=4,textvariable=comentario_create)
 comentario_text.grid(row=5,column=1,padx=10,columnspan=3)
 comentario_scroll = Scrollbar(frame,command=comentario_text.yview)
 comentario_scroll.grid(row=5,column=4,sticky="nsew")
@@ -67,10 +125,6 @@ update_button = Button(frame,text="update")
 update_button.grid(row=6,column=2,padx=10)
 delete_button = Button(frame,text="delete")
 delete_button.grid(row=6,column=3)
-
-
-
-
-
+#************************** CIERRES ***************************
 
 root.mainloop()
